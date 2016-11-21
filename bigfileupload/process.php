c3dee247cc76e9b40ee985d6527f116c2620c384
Himanshu <himanshu53939@gmail.com>
2016-11-21 19:03:38 +0530

    first

---
+++ bigfileupload/process.php
@@ -0,0 +1,16 @@
+<?php
+include 'vendor/autoload.php';
+
+use Dilab\Network\SimpleRequest;
+use Dilab\Network\SimpleResponse;
+use Dilab\Resumable;
+
+$request = new SimpleRequest();
+$response = new SimpleResponse();
+
+sleep(5);
+
+$resumable = new Resumable($request, $response);
+$resumable->tempFolder = 'tmps';
+$resumable->uploadFolder = 'uploads';
+$resumable->process();
